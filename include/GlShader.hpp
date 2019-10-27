3ec954914fe4f76acfa1b941595dbf7a24a39a46
RandomRandi <qqqplanshet74@gmail.com>
2019-10-27 18:15:05 +0300

    com

---
+++ include/GlShader.hpp
@@ -0,0 +1,52 @@
+
+#pragma once
+
+#include "glew.h"
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <math.h>
+#include "glm/glm.hpp"
+
+using std::string;
+class GlShader
+     {
+         public:
+        GlShader();
+         ~GlShader();
+        GLuint loadFiles(const string& vertex_file_name, const string& fragment_file_name);
+        GLuint load(const string& vertex_source, const string& fragment_source);
+        GLuint load(const GLchar* vertex_source, const GLchar* fragment_source);
+
+        void use();
+         GLuint getIDProgram() {return ShaderProgram;}
+        bool isLoad() {return ShaderProgram !=0;}
+        //! Attribute
+       GLint getAttribLocation (const GLchar* name)  const;
+         GLint getAttribLocation(const std::string& name) const;
+        //! Uniform get
+        GLint getUniformLocation (const GLchar* name) const;
+        GLint getUniformLocation (const std :: string& name) const;
+      //! Uniform set
+        //void setUniform (GLint location, const vec4& value);
+         //void setUniform (GLint location, const vec3& value);
+         //void setUniform (GLint location, const vec2& value);
+
+         //void setUniform (GLint location, const mat4& value);
+         void setUniform (GLint location, const GLint& value);
+
+         private:
+         void printInfoLogShader (GLuint shader);
+
+         void printInfoLogShader (GLuint shader);
+
+         GLuint loadSourcefile(const string& source_file_name, GLuint shader_type);
+
+         GLuint compileSource(const GLchar* source, GLuint shader_type);
+
+         void linkProgram();
+         GLuint ShaderProgram;
+         GLuint vertex_shader;
+        GLuint fragment_shader;
+    };
--- src/ShaderLoad.cpp
+++ src/ShaderLoad.cpp
@@ -5,64 +5,74 @@
 #include <string.h>
 #include <windows.h>
 #include <math.h>
+#include "GlShader.hpp"
 
-class Shader
+using std :: ifstream;
+
+GlShader::GlShader():ShaderProgram(0)
 {
-public:
-    // Идентификатор программы
-    GLuint Program;
-    // Конструктор считывает и собирает шейдер
-    Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
-    // Использование программы
-    void Use();
-};
 
-// #ifndef GLSHADER_H 
-// #define GLSHADER_H
-// using std::string;
-// //using namespace glm;
+}
+GlShader::~GlShader()
+{
+    glUseProgram(0);
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+    glDeleteShader(ShaderProgram);
+}
+GLuint GlShader :: loadFiles (const string& vertex_file_name, const string& fragment_file_name)
+{
+    vertex_shader = loadSourcefile (vertex_file_name, GL_VERTEX_SHADER);
+    fragment_shader = loadSourcefile (fragment_file_name, GL_FRAGMENT_SHADER);
+linkProgram();
+return ShaderProgram;
+}
+GLuint  GlShader ::load(const string& vertex_source, const string& fragment_source)
+{
+    vertex_shader = compileSource (vertex_source.c_str(), GL_VERTEX_SHADER);
+    fragment_shader = compileSource (fragment_source.c_str(), GL_FRAGMENT_SHADER);
+    linkProgram();
+    return ShaderProgram;
+}
+GLuint  GlShader ::load(const GLchar* vertex_source, const GLchar* fragment_source)
+{
+    vertex_shader = compileSource (vertex_source, GL_VERTEX_SHADER);
+    fragment_shader = compileSource (fragment_source, GL_FRAGMENT_SHADER);
+    linkProgram();
+    return ShaderProgram;
+}
 
-// class GlShader
-// {
-//     public:
-//     GlShader();
-//     ~GlShader();
-//     GLuint loadFiles(const string& vertex_file_name, const string& fragment_file_name);
-//     GLuint load(const string& vertex_source, const string& fragment_source);
-//     GLuint load(const GLchar* vertex_source, const GLchar* fragment_source);
-     
-//     void use();
-//     GLuint getIDProgram() {return ShaderProgram;}
-//     bool isLoad() {return ShaderProgram !=0;}
-//     //! Attribute
-//     GLint getAttribLocation (const Glchar* name)  const;
-//     GLint getAttribLocation(const std::string& name) const;
-//     //! Uniform get
-//     GLint getUniformLocation (const GLchar* name) const;
-//     GLint getUniformLocation (const std :: string& name) const;
-//     //! Uniform set
-//     void setUniform (GLint location, const vec4& value);
-//     void setUniform (GLint location, const vec3& value);
-//     void setUniform (GLint location, const vec2& value);
+void GlShader :: linkProgram ()
+{
+    GLint link_ok = GL_FALSE;
+    if (!vertex_shader || !fragment_shader)
+    {
+        ShaderProgram = GL_FALSE;
+        return;
+    } 
 
-//     void setUniform (GLint location, const mat4& value);
-//     void setUniform (GLint location, const GLint& value);
+ShaderProgram = glCreateProgram();
+glAttachShader(ShaderProgram, vertex_shader);
+glAttachShader(ShaderProgram, fragment_shader);
+glLinkProgram(ShaderProgram);
+glGetProgramiv (ShaderProgram, GL_LINK_STATUS, &link_ok);
+if (!link_ok) {
+    std::cout<< "glLinkProgram:";
+    //функция на вывод информации о линке 
 
-//     private:
-//     void printInfoLogShader (Gluint shader);
+}
 
-//     void printInfoLogShader (Gluint shader);
 
-//     GLuint loadSourcefile(const string& source_file_name, GLuint shader_type);
 
-//     GLuint compileSource(const GLchar* source, GLuint shader_type);
 
-//     void linkProgram();
-//     {GLuint ShaderProgram;
-//     GLuint vertex_shader;
-//     GLuint fragment_shafer;}
-// };
-// #endif
+
+
+
+
+
+
+
+
 
 
 

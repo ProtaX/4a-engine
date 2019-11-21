43037450df10a83be4589b4a6f24f5f07699bd59
Arseniy Karpenko <karpenko1298@gmail.com>
2019-11-21 23:13:14 +0300

    New tests

--- tests/VertexLayoutTest.cpp
+++ tests/VertexLayoutTest.cpp
@@ -2,55 +2,44 @@
 #include <catch2/catch.hpp>
 #include <VertexLayout.hpp>
 #include <VertexBuffer.hpp>
-#include <Camera.hpp>
 #include <stdio.h>
 
 using namespace fae;
 
 TEST_CASE("Eq class", "[VertexLayout]") {
+    //Arrange
     VertexLayout vl(0);
-    //Внутри эти push образуют класс эквивалентности {3, 3, 2}
-    vl.Push<float>(3);
-    vl.Push<float>(3);
-    vl.Push<float>(2);
-
-    //Создадим такую же структуру вручную
     std::vector<struct VertexBufferAttributes> test_struct = {{GL_FLOAT, 3, GL_FALSE}, 
                                                               {GL_FLOAT, 3, GL_FALSE}, 
                                                               {GL_FLOAT, 2, GL_FALSE}};
     auto created_struct = vl.GetAttributes();
-
-    //Сравним результаты
+    //Action
+    vl.Push<float>(3);
+    vl.Push<float>(3);
+    vl.Push<float>(2);
+    //Assert
     for (int i = 0; i < 3; i++) {
         REQUIRE(memcmp(&test_struct[i], &created_struct[i], sizeof(struct VertexBufferAttributes)) == 0);
     }
 }
 
 TEST_CASE("Border values", "[VertexLayout]") {
+    //Arrange
     VertexLayout vl(0);
+    //Action
     vl.Push<float>(-1);
-
-    //-1 - некорректное граничное значение
+    //Assert
     REQUIRE(vl.GetStride() < 0);
 }
 
 TEST_CASE("Cause and effect analysis", "[VertexLayout]") {
+    //Arrange
     VertexLayout vl(0);
+    //Action
     vl.Push<float>(3);
     vl.Push<float>(3);
     vl.Push<float>(2);
-
-    //Добавляется 8 элементов типа float, следовательно, и размер (stride)
-    //будет равен 8 * sizeof(float)
+    //Assert
     REQUIRE(vl.GetStride() == 8 * sizeof(float));
 }
 
-TEST_CASE("Error prediction", "[Camera]") {
-    //Предскажем ошибку: пользователь перепутал тип параметров
-    int windowW = 1024, windowH = 720;
-    Camera orthCam(windowW, windowH);
-
-    //Сравним его с приером, в котором параметры парвильные
-    Camera orthCam_right(1024., 720.);
-    REQUIRE(orthCam.GetProj() == orthCam_right.GetProj());
-}

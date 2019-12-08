7c80d96e908b20ce4182cdb7b6acef96771b7165
Arseniy Karpenko <karpenko1298@gmail.com>
2019-12-08 22:15:21 +0300

    SOIL lib added as a submodule

--- variables.cmake
+++ variables.cmake
@@ -8,7 +8,7 @@ set (INCLUDE_DIR ${PROJECT_DIR}/include)
 set (GLM_INCLUDE_DIR ${PROJECT_DIR}/externals/glm)
 set (GLEW_INCLUDE_DIR ${PROJECT_DIR}/externals/glew2_protax/include)
 set (GLFW_INCLUDE_DIR ${PROJECT_DIR}/externals/glfw/include)
-set (SOIL_INCLUDE_DIR ${PROJECT_DIR}/externals/soil)
+set (SOIL_INCLUDE_DIR ${PROJECT_DIR}/externals/soil/inc/SOIL)
 set (CATCH2_INCLUDE_DIR ${PROJECT_DIR}/externals/Catch2/include)
 set (CATCH2_SINGLE_INCLUDE_DIR ${PROJECT_DIR}/externals/Catch2/single_include)
 
@@ -20,5 +20,3 @@ set (TESTS_DIR ${PROJECT_DIR}/tests)
 set (SRC_DIR ${PROJECT_DIR}/src)
 
 set (MAIN_SRC ${SRC_DIR}/main.cpp)
-
-set (SOIL_STATIC_LIB ${EXTERNALS_DIR}/soil/libSOIL.a)
